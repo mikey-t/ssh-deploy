dad0c40cf45bfb46b3d3fdc9130c0fe57e9d5dee
Mike Thompson <mikeylikes23@gmail.com>
2021-11-22 22:48:35 -0700

    Setup for publishing

---
+++ index.js
@@ -0,0 +1,196 @@
+const fs = require('fs-extra')
+const { NodeSSH } = require('node-ssh')
+
+class SshDeploy {
+  serverAddress
+  username
+  privateKeyFilePath
+  ssh
+
+  constructor(serverAddress, username, privateKeyFilePath) {
+    if (!serverAddress || !username || !privateKeyFilePath) {
+      throw new Error('all params are required')
+    }
+    
+    this.serverAddress = serverAddress
+    this.username = username
+    this.privateKeyFilePath = privateKeyFilePath
+    this.ssh = new NodeSSH()
+  }
+
+  async connect() {
+    console.log(`attempting to ssh to ${this.username}@${this.serverAddress} using key file ${this.privateKeyFilePath}`)
+    
+    if (!fs.existsSync(this.privateKeyFilePath)) {
+      throw new Error('could not find key file ' + this.privateKeyFilePath)
+    }
+    
+    await this.ssh.connect({
+      host: this.serverAddress,
+      username: this.username,
+      privateKey: this.privateKeyFilePath
+    })
+    
+    if (!this.ssh.isConnected()) {
+      throw new Error('error: connecting to remote host failed')
+    }
+  }
+  
+  // This seems very fragile but should be an ok start...
+  async getServiceStatus(serviceName) {
+    if (!serviceName) {
+      throw new Error('serviceName is required')
+    }
+    
+    console.log(`checking if service ${serviceName} is active`)
+    
+    const result = await this.ssh.execCommand('sudo systemctl is-active ' + serviceName)
+    
+    return result.stdout
+  }
+
+  async stopService(serviceName) {
+    if (!serviceName) {
+      throw new Error('serviceName is required')
+    }
+
+    const status = await this.getServiceStatus(serviceName)
+    
+    if (status === 'activating') {
+      throw new Error('service status is activating - let\'s not try stopping it in this state')
+    }
+    
+    if (status === 'inactive') {
+      console.log('service is already stopped, no action taken')
+      return
+    }
+    
+    console.log(`stopping service ${serviceName} on server ${this.serverAddress}`)
+    
+    const result = await this.ssh.execCommand(`sudo systemctl is-active --quiet ${serviceName} && sudo systemctl stop ${serviceName}`)
+    
+    if (result.code !== 0) {
+      console.log('command failed - stdout:')
+      console.log(result)
+      throw new Error('stopService failed')
+    }
+  }
+  
+  async startService(serviceName) {
+    if (!serviceName) {
+      throw new Error('serviceName is required')
+    }
+    
+    console.log(`starting service ${serviceName} on server ${this.serverAddress}`)
+    
+    const result = await this.ssh.execCommand('sudo systemctl start ' + serviceName)
+    
+    if (result.code !== 0) {
+      console.log('command failed - stdout:')
+      console.log(result)
+      throw new Error('startService failed')
+    }
+  }
+  
+  async transferFileToServer(localFilePath, serverFilePath) {
+    if (!localFilePath) {
+      throw new Error('localFilePath is required')
+    }
+    
+    if (!fs.existsSync(localFilePath)) {
+      throw new Error('path does not exist for localFilePath: ' + localFilePath)
+    }
+    
+    console.log(`attempting to send file ${localFilePath} to ${serverFilePath}`)
+    
+    await this.ssh.putFile(localFilePath, serverFilePath)
+    
+    // An error would have been thrown if not successful
+    console.log('successfully transferred file')
+  }
+  
+  // Suggested serverUnpackDirectoryOwner: www-data
+  // For now enforcing serverUnpackDirectory must have 'approot' in it because of how dangerous the command is...
+  async unpackTarball(serverTarballPath, serverUnpackDirectory, serverUnpackDirectoryOwner, numPathComponentsToStrip) {
+    if (!serverTarballPath || !serverUnpackDirectory || !serverUnpackDirectoryOwner) {
+      throw new Error('serverTarballPath and serverUnpackDirectory and serverUnpackDirectoryOwner are required')
+    }
+    
+    if (serverUnpackDirectory.indexOf('approot') === -1) {
+      throw new Error('error: serverUnpackDirectory must contain approot')
+    }
+    
+    const ensureUnpackDirCommand = `sudo -u ${serverUnpackDirectoryOwner} mkdir -p ${serverUnpackDirectory} -m 0755`
+    
+    console.log('ensuring directory exists:  ' + serverUnpackDirectory)
+    
+    const ensureDirResult = await this.ssh.execCommand(ensureUnpackDirCommand)
+    
+    if (ensureDirResult.code !== 0) {
+      throw new Error('error: could not create serverUnpackDirectory ' + serverUnpackDirectory + '. result: ' + JSON.stringify(ensureDirResult))
+    }
+    
+    const deleteExistingFilesCommand = `sudo rm -rf ${serverUnpackDirectory}/*`
+    
+    console.log('deleting existing files from server app directory: ' + serverUnpackDirectory)
+    
+    const deleteExistingResult = await this.ssh.execCommand(deleteExistingFilesCommand)
+    
+    if (!deleteExistingResult.code === 0) {
+      throw new Error('error: could not delete existing files from serverUnpackDirectory. result: ' + JSON.stringify(deleteExistingResult))
+    }
+    
+    const unpackCommand = `sudo tar -xf ${serverTarballPath} -C ${serverUnpackDirectory} --strip-components=${numPathComponentsToStrip || 0}`
+    
+    const unpackResult = await this.ssh.execCommand(unpackCommand)
+    
+    if (unpackResult.code !== 0) {
+      throw new Error('error: could not unpack. result:' + JSON.stringify(unpackResult))
+    }
+  }
+  
+  // Suggested owner and group: www-data
+  // Suggested permissions: '0755'
+  async recursivelyChangeOwnerAndPermissions(serverDirectory, owner, group, permissions) {
+    if (!serverDirectory || !owner || !group || !permissions) {
+      throw new Error('all params are required')
+    }
+    
+    console.log('running chown command')
+    
+    const chownCommand = `sudo chown -R ${owner}:${group} ${serverDirectory}`
+    const chownResult = await this.ssh.execCommand(chownCommand)
+    
+    if (chownResult.code !== 0) {
+      throw new Error('error: chown failed. result: ' + JSON.stringify(chownResult))
+    }
+    
+    console.log('running chmod command')
+    
+    const chmodCommand = `sudo chmod -R ${permissions} ${serverDirectory}`
+    
+    const chmodResult = await this.ssh.execCommand(chmodCommand)
+    
+    if (chmodResult.code !== 0) {
+      throw new Error('error: chmod failed. result: ' + JSON.stringify(chmodResult))
+    }
+  }
+  
+  async simpleFileDeploy(serviceName, localTarballPath, serverTempDir, tarballName, serverAppDir, serverAppOwner, numPathStripComponents, appDirPermissions) {
+    if (!this.ssh.isConnected()) {
+      throw new Error('error: ssh client is not connected - call await connect() method before this method')
+    }
+    
+    await this.stopService(serviceName)
+    await this.transferFileToServer(localTarballPath, `${serverTempDir}/${tarballName}`)
+    await this.unpackTarball(`${serverTempDir}/${tarballName}`, serverAppDir, serverAppOwner, numPathStripComponents)
+    await this.recursivelyChangeOwnerAndPermissions(serverAppDir, serverAppOwner, serverAppOwner, appDirPermissions)
+    await this.startService(serviceName)
+  }
+
+  disconnect() {
+    this.ssh.dispose()
+  }
+}
+
+module.exports.SshDeploy = SshDeploy
---
+++ readme.md
@@ -0,0 +1,17 @@
+# @mikeyt23/ssh-deploy
+
+> :warning: **DANGER!** There be dragons here. This package can do serious damage if you misuse it. Or maybe even if you use it properly. 
+
+Don't store any ssh private keys in your project - pull from outside your project directory. Don't check in any secrets or any server info to any of your repositories. Use environment variables or a secret vault or some other mechanism that doesn't require you to expose sensitive information.
+
+You've been warned.
+
+## How
+
+`npm i -D @mikeyt23/ssh-deploy`
+
+Write a script that looks something like this:
+
+```JavaScript
+// TODO
+```
